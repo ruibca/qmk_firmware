0a29c86c540e71cabf424f0d2b7d88a448791685
Rui Carvalho <ruibca@gmail.com>
2018-12-13 23:23:06 +0000

    Moved symbols.

--- keyboards/planck/keymaps/ruibca/keymap.c
+++ keyboards/planck/keymaps/ruibca/keymap.c
@@ -119,17 +119,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  Del |   !  |   "  |      |   $  |   %  |   ^  |   &  |   *  |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  |   (  |   )  |   {  |   }  |   @  | Left | Down |  Up  |Right |      |      |
+ * |      |   (  |   )  |   {  |   }  |   _  | Left | Down |  Up  |Right |  @   |   ~  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |   |  |   \  |   _  |   [  |   ]  |   #  | Home |Pg Dn |Pg Up | End  |      |      |
+ * |   |  |   \  |      |   [  |   ]  |      | Home |Pg Dn |Pg Up | End  |      |   #  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Mute | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_DEL,  KC_EXLM, KC_DQUO, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______,
-    KC_TILD, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_AT,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, 
-    KC_PIPE, KC_BSLS, KC_UNDS, KC_LBRC, KC_RBRC, KC_HASH, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
+    _______, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_UNDS,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_AT  , KC_TILD, 
+    KC_PIPE, KC_BSLS, _______, KC_LBRC, KC_RBRC, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_HASH,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
